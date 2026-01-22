f042fe142b2b85ef1a00a7a61aa743bf2869711e
hmrahim <h.m.rahimnet@gmail.com>
2026-01-22 16:47:47 +0300

    fuck the env

--- middleware/sendEmail.js
+++ middleware/sendEmail.js
@@ -10,10 +10,10 @@ export const sendEmail = async ({ name, email, phone, message }) => {
 
     auth: {
       // আপনার gmail
-      user: process.env.EMAIL_USER,
+      user: "hmrahimdb@gmail.com",
 
       // gmail app password
-      pass: process.env.EMAIL_PASS,
+      pass: "lnrnqbbnahwfmyvb",
     },
   });
 
